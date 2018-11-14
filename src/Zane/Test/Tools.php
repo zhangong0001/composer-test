1a19e7b65b38457c1ae022e16b35c3a9850e4fa3
long <long@bogon.beijing.objectiva.local>
2018-11-14 22:21:15 +0800

    update

---
+++ src/Zane/Test/Tools.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zane\Test;
+
+class Tools 
+{
+    public function __construct()
+    {
+    
+    }
+
+    public function Hello()
+    {
+        echo "Hello World!";
+    }
+}
